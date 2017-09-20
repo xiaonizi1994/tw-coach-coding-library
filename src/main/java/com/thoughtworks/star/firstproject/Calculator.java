c79dbb76f52dacabd2a63b3d5c150dd5b540d3cc
YuanShenjian <sjyuan@thoughtworks.com>
2017-09-21 07:30:25 +0800

    Show first tdd case

--- src/main/java/com/thoughtworks/star/firstproject/Calculator.java
+++ src/main/java/com/thoughtworks/star/firstproject/Calculator.java
@@ -9,17 +9,4 @@ public class Calculator {
     public double sum(double number1, double number2) {
         return number1 + number2;
     }
-
-    public Map<String, Integer> sortAndCount(List<String> strings) {
-        Collections.sort(strings);
-        Map<String, Integer> result = new LinkedHashMap<>();
-        strings.forEach(item -> result.put(item, item.length()));
-        return result;
-    }
-
-    public int sumEvens(List<Integer> integers) {
-        return integers.stream()
-                .filter(number -> number % 2 == 0)
-                .mapToInt(Integer::intValue).sum();
-    }
 }
--- src/test/java/com/thoughtworks/star/firstproject/CalculatorTest.java
+++ src/test/java/com/thoughtworks/star/firstproject/CalculatorTest.java
@@ -2,10 +2,15 @@ package com.thoughtworks.star.firstproject;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
+import java.util.Random;
+import java.util.logging.Logger;
 
 import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.containsInAnyOrder;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 
@@ -24,33 +29,5 @@ public class CalculatorTest {
 
         // 3. 断言
         assertThat(result, is(14.0));
-    }
-
-    /**
-     * 统计列表中偶数的和
-     * Input: Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
-     * Output: 30
-     */
-    @Test
-    public void should_sum_evens() {
-        Calculator calculator = new Calculator();
-        int result = calculator.sumEvens(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
-        assertThat(result, is(30));
-    }
-
-
-    /**
-     * 按照名字拼音顺序统计名字的长度，输入为名字列表，输出为名字-长度映射
-     * Input: Arrays.asList("ynpan", "yzqi", "ybowang", "qiqzhao", "yibtan", "abc", "sjyuan")
-     * Output: Map<String, Integer>: {"abc": 3, qiqzhao": 7, "sjyuan": 6, "ybowang": 7, yibtan: 6, ynpan: 5, yzqi: 4 }
-     */
-    @Test
-    public void should_sort_and_count_names() {
-        Calculator calculator = new Calculator();
-
-        Map<String, Integer> result = calculator.sortAndCount(Arrays.asList("ynpan", "yzqi", "ybowang", "qiqzhao",
-                "yibtan", "abc", "sjyuan"));
-        assertThat(result.keySet(), contains("abc", "qiqzhao", "sjyuan", "ybowang", "yibtan", "ynpan", "yzqi"));
-        assertThat(result.values(), contains(3, 7, 6, 7, 6, 5, 4));
     }
 }
